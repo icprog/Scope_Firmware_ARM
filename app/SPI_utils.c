35f3370919daafc2d6ef9bfe1ac1889d61167360
Richard <richard@kspresearch.com>
2016-06-06 20:15:02 -0600

    Implemented SPI slave functionality.

--- app/SPI_utils.c
+++ app/SPI_utils.c
@@ -1,3 +1,34 @@
+/*******************************************************************************
+  SPI_utils: SPI Master and Slave utilities
+  
+  Company:
+    AvaTech
+  
+  File Name:
+    SPI_utils.c
+		
+  Summary:
+    SPI Master and Slave utilities.
+		
+  Description:
+    This file contains the initialization, configuration and handlers for the SPI master and slave module.
+     
+  Processor:       
+    nRF51822
+		
+  Toolchaing:
+		MDK-Lite version 5.20
+ 
+  Author(s): 
+    Richard Kirby
+ 
+  Created on:
+    June 1, 2016
+  
+  Revision History:
+    Development version      June 1, 2016
+ *******************************************************************************/
+ 
 #include "SPI_utils.h"
 #include <stdbool.h>
 #include <stdint.h>
@@ -7,8 +38,8 @@
 #include "nrf_delay.h"
 #include "nrf.h"
 #include "bsp.h"
-
 #include "nrf_drv_spi.h"
+#include "nrf_drv_spis.h"
 #include "app_util_platform.h"
 #include "nrf_gpio.h"
 #include "nrf_log.h"
@@ -16,10 +47,13 @@
 #include <string.h>
 
 static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(SPI_INSTANCE);  /**< SPI instance. */
+static const nrf_drv_spis_t spis = NRF_DRV_SPIS_INSTANCE(SPIS_INSTANCE);/**< SPIS instance. */
+nrf_drv_spis_config_t spis_config = NRF_DRV_SPIS_DEFAULT_CONFIG(SPIS_INSTANCE);
 static volatile bool spi_xfer_done;  /**< Flag used to indicate that SPI instance completed the transfer. */
-static uint8_t       m_tx_buf[2];
-static uint8_t       m_rx_buf[2];
-static const uint8_t m_length = sizeof(m_tx_buf);        /**< Transfer length. */
+volatile bool spis_xfer_done; /**< Flag used to indicate that SPIS instance completed the transfer. */
+uint8_t       m_tx_buf_s[2];
+uint8_t       m_rx_buf_s[3];
+static const uint8_t m_length = sizeof(m_tx_buf_s);        /**< Transfer length. */
 
 /**
  * @brief SPI user event handler.
@@ -30,10 +64,31 @@ void spi_event_handler(nrf_drv_spi_evt_t const * p_event)
     spi_xfer_done = true;
 }
 
+/**
+ * @brief SPIS user event handler.
+ *
+ * @param event
+ */
+void spis_event_handler(nrf_drv_spis_event_t event)
+{
+    if (event.evt_type == NRF_DRV_SPIS_XFER_DONE)
+    {
+        spis_xfer_done = true;
+				if (nrf_drv_spis_buffers_set(&spis, m_tx_buf_s, m_length, m_rx_buf_s, m_length) != NRF_SUCCESS)
+						LEDS_ON(BSP_LED_3_MASK);
+				LEDS_INVERT(BSP_LED_2_MASK);
+    }
+}
+
+/**
+ * @brief SPI Master Initialization
+ *
+ */
 void spi_init(void)
 {
 		nrf_drv_spi_config_t spi_config = NRF_DRV_SPI_DEFAULT_CONFIG(SPI_INSTANCE);
 		spi_config.ss_pin = SPI_CS_PIN;
+	  /* TODO(rk): turn off CS pin in master SPI driver */
 		spi_config.mode = NRF_DRV_SPI_MODE_3;
 		APP_ERROR_CHECK(nrf_drv_spi_init(&spi, &spi_config, spi_event_handler));
 		
@@ -43,13 +98,43 @@ void spi_init(void)
 	  NRF_GPIO->OUTSET = (1<<SPI_CS_ACC);
 	  NRF_GPIO->OUTSET = (1<<SPI_CS_GYRO);
 		
-		printf("\n\rSPI Master Configuration:");
-		printf("\n\r  SCK pin: %x", spi_config.sck_pin);
-		printf("\n\r  MOSI pin: %x", spi_config.mosi_pin);
-		printf("\n\r  MISO pin: %x", spi_config.miso_pin);
-		printf("\n\r  Chip Select pin:w %x", spi_config.ss_pin);
-		printf("\n\r  SPI Mode: %x", spi_config.mode);
+		printf("\n\r\n\rSPI Master Configuration:");
+		printf("\n\r  SCK pin: %d", spi_config.sck_pin);
+		printf("\n\r  MOSI pin: %d", spi_config.mosi_pin);
+		printf("\n\r  MISO pin: %d", spi_config.miso_pin);
+		printf("\n\r  Chip Select pin: %d", spi_config.ss_pin);
+		printf("\n\r  SPI Mode: %d", spi_config.mode);
 }
+
+
+/**
+ * @brief SPI Slave Initialization
+ *
+ */
+
+void spis_init(void)
+{
+	  spis_config.csn_pin               = SPIS_CS_PIN;
+		spis_config.mode 									= NRF_DRV_SPIS_MODE_3;
+	
+	  printf("\n\r\n\rSPI Slave Configuration:");
+		printf("\n\r  SCK pin: %d", spis_config.sck_pin);
+		printf("\n\r  MOSI pin: %d", spis_config.mosi_pin);
+		printf("\n\r  MISO pin: %d", spis_config.miso_pin);
+		printf("\n\r  Chip Select pin: %d", spis_config.csn_pin);
+		printf("\n\r  SPI Mode: %d", spis_config.mode);
+	
+		if (nrf_drv_spis_init(&spis, &spis_config, spis_event_handler) == NRF_SUCCESS)
+				printf("\n\rSPI Slave Initialization Succeded");
+		else
+			printf("\n\rSPI Slave Initialization Failed");
+				
+		if (nrf_drv_spis_buffers_set(&spis, m_tx_buf_s, m_length, m_rx_buf_s, m_length) == NRF_SUCCESS)
+			  printf("\n\rSPI Slave Buffer Set Succeded");
+		else
+			printf("\n\rSPI Slave Buffer Set Failed");
+}
+
 
 uint8_t SPIReadByte(uint8_t address, SPI_DEVICE device)
 {
--- app/SPI_utils.h
+++ app/SPI_utils.h
@@ -1,12 +1,47 @@
+/*******************************************************************************
+  SPI_utils: SPI Master and Slave utilities header
+  
+  Company:
+    AvaTech
+  
+  File Name:
+    SPI_utils.h
+		
+  Summary:
+    SPI Master and Slave utilities header.
+		
+  Description:
+    This file contains the function prototypes and definitions for teh SPI master and slave
+		initialization, configuration and handlers.
+     
+  Processor:       
+    nRF51822
+		
+  Toolchaing:
+		MDK-Lite version 5.20
+ 
+  Author(s): 
+    Richard Kirby
+ 
+  Created on:
+    June 1, 2016
+  
+  Revision History:
+    Development version      June 1, 2016
+ *******************************************************************************/
+ 
 #ifndef SPI_UTILS_H
 #define	SPI_UTILS_H
 
 #include <stdint.h>
+#include "nrf_drv_spis.h"
 
 #define SPI_CS_PIN   7
+#define SPIS_CS_PIN 12
 #define SPI_CS_ACC	 5
 #define SPI_CS_GYRO  6
 #define SPI_INSTANCE  0 /**< SPI instance index. */
+#define SPIS_INSTANCE 1 /**< SPIS instance index. */
 
 typedef enum
 {
@@ -19,6 +54,8 @@ uint8_t getLSMID(void);
 uint8_t SPIReadByte(uint8_t address, SPI_DEVICE device);
 void SPIWriteReg(uint8_t address, uint8_t regVal, SPI_DEVICE device);
 void SPIReadMultipleBytes(uint8_t address, uint8_t * tx_buf, uint8_t * rx_buf, uint8_t length);
+void spis_init(void);
+void spis_event_handler(nrf_drv_spis_event_t event);
 
 #endif	/* SPI_UTILS_H */
 
