5d3111f0624d114e3d5af9f9bf91cbef9e5ab97a
jtrovato <josephjtrovato@gmail.com>
2016-06-24 14:22:55 -0600

    setting pins for SPI slave module

--- app/SPI_utils.h
+++ app/SPI_utils.h
@@ -36,8 +36,8 @@
 #include <stdint.h>
 #include "nrf_drv_spis.h"
 
-#define SPI_CS_PIN   7
-#define SPIS_CS_PIN 12
+#define SPI_CS_PIN   7 //chips select for the SPI module for the IMU
+#define SPIS_CS_PIN 10 //REQN on ARM, RDYN on PIC
 #define SPI_CS_ACC	 2
 #define SPI_CS_GYRO  15 // TODO: change for our ble pcb
 #define SPI_INSTANCE  0 /**< SPI instance index. */
