d963f5ab05b5e5e504aeebae0675715d43e76110
jtrovato <josephjtrovato@gmail.com>
2016-07-29 16:31:31 -0600

    merges always fuck shit up

--- app/main.c
+++ app/main.c
@@ -1107,15 +1107,13 @@ int main(void)
 	
     // Start execution.
     application_timers_start();
-    
-    
     err_code = ble_advertising_start(BLE_ADV_MODE_FAST);
     APP_ERROR_CHECK(err_code);
     
 	//init_LSM303();
 	APP_Initialize();
 	
-	send_data_to_PIC(get_profile_pack); //test: send profile pack to PIC
+	//send_data_to_PIC(get_profile_pack); //test: send profile pack to PIC
 	SEGGER_RTT_WriteString(0, "main loop:\n");
 
 	kk = 0;
@@ -1135,14 +1133,14 @@ int main(void)
 		kk++;
 		send_data[0] = kk;
 		
-        nrf_delay_ms(1000);
+        //nrf_delay_ms(1000);
         //ble_probe_error_update(&m_pes, kk);
 		//profile_data_update(&m_ps, send_data, 10);
 
 		if(kk >=10) kk=0;
 
        APP_Tasks();
-        power_manage();
+        //power_manage();
 			//SEGGER_RTT_WriteString(0, "looping....\n");
 	}
 
