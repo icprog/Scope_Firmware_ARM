c611223090f55afb0ba35d47af14e9e6f153b59f
jtrovato <josephjtrovato@gmail.com>
2016-08-24 11:27:41 -0600

    actually adding timer files

---
+++ app/timers.h
@@ -0,0 +1,34 @@
+#ifndef TIMERS_H__
+#define TIMERS_H__
+
+#include "app_timer.h"
+
+
+/*************************   DEFINITION   *************************/
+
+#define APP_TIMER_PRESCALER                  3    /**< Value of the RTC1 PRESCALER register. */
+#define APP_TIMER_OP_QUEUE_SIZE              4    /**< Size of timer operation queues. */
+
+#define battery_LEVEL_MEAS_INTERVAL          APP_TIMER_TICKS(2000, APP_TIMER_PRESCALER) /**< Battery level measurement interval (ticks). */                         
+#define slope_LEVEL_MEAS_INTERVAL          	 APP_TIMER_TICKS(500, APP_TIMER_PRESCALER) /**< slope level measurement interval (ticks). */
+#define status_LEVEL_MEAS_INTERVAL           APP_TIMER_TICKS(2000, APP_TIMER_PRESCALER) /**< status level measurement interval (ticks). */
+#define acc_LEVEL_MEAS_INTERVAL              APP_TIMER_TICKS(2, APP_TIMER_PRESCALER)
+
+
+//defines variables to be used for app timers
+APP_TIMER_DEF(m_acc_timer_id);
+APP_TIMER_DEF(m_slope_timer_id);
+APP_TIMER_DEF(m_battery_timer_id);     /**< Battery timer. */
+APP_TIMER_DEF(m_status_timer_id);      /**< Status timer. */
+
+/*************************  FUNCTIONS   ***************************/
+void timers_init(void);
+void application_timers_start(void);
+void application_timers_stop(void);
+void acc_timeout_handler(void *p_context);
+void battery_timeout_handler(void *p_context);
+void slope_timeout_handler(void *p_context);
+void status_timeout_handler(void *p_context);
+
+
+#endif //TIMERS_H__
